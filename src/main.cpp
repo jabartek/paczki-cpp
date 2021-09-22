92680433454032acb7226a65f981eac32be0403d
Bartlomiej Janowski <bartlomiej.janowski@neontri.com>
2021-09-22 18:30:27 +0200

    Dodanie assetów i niewielkie zmiany cośtam cośtam

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <limits>
 #include <raylib.h>
 #include <raymath.h>
 
@@ -26,8 +27,9 @@ struct CubeCC {
 
 using json = nlohmann::json;
 
-int main(int argc, char* argv[]) {
-  if (argc < 2) return 1;
+int main(int argc, char *argv[]) {
+  if (argc < 2)
+    return 1;
 
   json data;
   std::filesystem::path file_path(argv[1]);
@@ -48,12 +50,13 @@ int main(int argc, char* argv[]) {
     };
   }
 
-  for (auto& [key, value] : box_types) {
+  for (auto &[key, value] : box_types) {
     std::cout << value.ref << "\t" << value.x_size << "\t" << value.y_size
               << "\t" << value.z_size << std::endl;
   }
   for (auto i = 0; i < 1000; ++i) {
-    if (!box_types.contains(i)) continue;
+    if (!box_types.contains(i))
+      continue;
     std::cout << i << std::endl;
   }
 
@@ -85,67 +88,56 @@ int main(int argc, char* argv[]) {
     cubes.push_back(cube);
   }
 
-  //   std::generate_n(std::back_inserter(cubes), 20, []() {
-  //     CubeCC cube;
-  //     float* pos = reinterpret_cast<float*>(&cube);
-  //     for (auto npos = pos; npos < pos + 3; ++npos) {
-  //       *npos = static_cast<float>(rand() % 20 - 10);
-  //     }
-  //     for (auto npos = pos + 3; npos < pos + 6; ++npos) {
-  //       *npos = static_cast<float>(rand() % 5 + 1);
-  //     }
-  //     cube.color = {static_cast<uint8_t>(rand() % 256),
-  //                   static_cast<uint8_t>(rand() % 256),
-  //                   static_cast<uint8_t>(rand() % 256), 255};
-  //     return cube;
-  //   });
-
-  // Initialization
-  //--------------------------------------------------------------------------------------
   const int screenWidth = 800;
   const int screenHeight = 450;
 
-  InitWindow(screenWidth, screenHeight,
-             "raylib [models] example - mesh picking");
+  InitWindow(screenWidth, screenHeight, "Paczki C++");
 
-  // Define the camera to look into our 3d world
   Camera camera = {0};
-  camera.position = {20.0f, 20.0f, 20.0f};  // Camera position
-  camera.target = {0.0f, 8.0f, 0.0f};       // Camera looking at point
-  camera.up = {0.0f, 1.6f, 0.0f};  // Camera up vector (rotation towards target)
-  camera.fovy = 45.0f;             // Camera field-of-view Y
-  camera.projection = CAMERA_PERSPECTIVE;  // Camera mode type
+  camera.position = {20.0f, 20.0f, 20.0f};
+  camera.target = {0.0f, 8.0f, 0.0f};
+  camera.up = {0.0f, 1.6f, 0.0f};
+  camera.fovy = 45.0f;
+  camera.projection = CAMERA_PERSPECTIVE;
 
-  Ray ray = {0};  // Picking ray
+  Ray ray = {0};
 
-  // Ground quad
   Vector3 g0 = {-50.0f, 0.0f, -50.0f};
   Vector3 g1 = {-50.0f, 0.0f, 50.0f};
   Vector3 g2 = {50.0f, 0.0f, 50.0f};
   Vector3 g3 = {50.0f, 0.0f, -50.0f};
 
-  SetCameraMode(camera, CAMERA_ORBITAL);  // Set a free camera mode
+  SetCameraMode(camera, CAMERA_ORBITAL);
 
-  SetTargetFPS(60);  // Set our game to run at 60 frames-per-second
-  //--------------------------------------------------------------------------------------
-  // Main game loop
-  while (!WindowShouldClose())  // Detect window close button or ESC key
-  {
-    // Update
-    //----------------------------------------------------------------------------------
-    UpdateCamera(&camera);  // Update camera
+  SetTargetFPS(60);
 
-    // Display information about closest hit
-    RayCollision collision = {0};
-    std::string hitObjectName = "None";
-    collision.distance = FLT_MAX;
+  auto mul = [](const Vector3 &vecs, auto f) -> Vector3 {
+    Vector3 vec;
+    vec.x = vecs.x * f;
+    vec.y = vecs.y * f;
+    vec.z = vecs.z * f;
+    return vec;
+  };
+
+  auto plus = [](const Vector3 &vec1, const Vector3 &vec2) -> Vector3 {
+    Vector3 vec;
+    vec.x = vec1.x + vec2.x;
+    vec.y = vec1.y + vec2.y;
+    vec.z = vec1.z + vec2.z;
+    return vec;
+  };
+
+  while (!WindowShouldClose()) {
+    UpdateCamera(&camera);
+
+    RayCollision collision{};
+    std::string hitObjectName("None");
+    collision.distance = std::numeric_limits<float>::max();
     collision.hit = false;
     Color cursorColor = WHITE;
 
-    // Get ray and test against objects
     ray = GetMouseRay(GetMousePosition(), camera);
 
-    // Check ray collision against ground quad
     RayCollision groundHitInfo = GetRayCollisionQuad(ray, g0, g1, g2, g3);
 
     if ((groundHitInfo.hit) && (groundHitInfo.distance < collision.distance)) {
@@ -154,25 +146,10 @@ int main(int argc, char* argv[]) {
       hitObjectName = "Ground";
     }
 
-    auto mul = [](const Vector3& vecs, auto f) -> Vector3 {
-      Vector3 vec;
-      vec.x = vecs.x * f;
-      vec.y = vecs.y * f;
-      vec.z = vecs.z * f;
-      return vec;
-    };
-
-    auto plus = [](const Vector3& vec1, const Vector3& vec2) -> Vector3 {
-      Vector3 vec;
-      vec.x = vec1.x + vec2.x;
-      vec.y = vec1.y + vec2.y;
-      vec.z = vec1.z + vec2.z;
-      return vec;
-    };
-    CubeCC* hitCube(nullptr);
+    CubeCC *hitCube(nullptr);
     std::list<CubeCC>::iterator hitCubeIt;
     for (auto it = cubes.begin(); it != cubes.end(); ++it) {
-      auto& cube = *it;
+      auto &cube = *it;
       RayCollision cubeHitInfo =
           GetRayCollisionBox(ray, {cube.pos, plus(cube.pos, cube.size)});
       if ((cubeHitInfo.hit) && (cubeHitInfo.distance < collision.distance)) {
@@ -184,24 +161,19 @@ int main(int argc, char* argv[]) {
       }
     }
 
-    if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && hitCube) {
+    if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && hitCube) {
       cubes.erase(hitCubeIt);
     }
 
-    // Draw
-    //----------------------------------------------------------------------------------
     BeginDrawing();
-
     ClearBackground(RAYWHITE);
-
     BeginMode3D(camera);
 
-    for (auto& cube : cubes) {
+    for (auto &cube : cubes) {
       DrawCube(plus(cube.pos, mul(cube.size, 0.5f)), cube.size.x, cube.size.y,
                cube.size.z, cube.color);
     }
 
-    // If we hit something, draw the cursor at the hit point
     if (collision.hit) {
       DrawCube(collision.point, 0.3f, 0.3f, 0.3f, cursorColor);
       DrawCubeWires(collision.point, 0.3f, 0.3f, 0.3f, RED);
@@ -228,7 +200,6 @@ int main(int argc, char* argv[]) {
 
     EndMode3D();
 
-    // Draw some debug GUI text
     DrawText(TextFormat("Hit Object: %s", hitObjectName.c_str()), 10, 50, 10,
              BLACK);
 
@@ -247,19 +218,12 @@ int main(int argc, char* argv[]) {
                10, ypos + 30, 10, BLACK);
     }
 
-    DrawText("Use Mouse to Move Camera", 10, 430, 10, GRAY);
-
-    DrawText("(c) Turret 3D model by Alberto Cano", screenWidth - 200,
-             screenHeight - 20, 10, GRAY);
-
     DrawFPS(10, 10);
 
     EndDrawing();
-    //----------------------------------------------------------------------------------
   }
 
-  CloseWindow();  // Close window and OpenGL context
-  //--------------------------------------------------------------------------------------
+  CloseWindow();
 
   return 0;
 }
